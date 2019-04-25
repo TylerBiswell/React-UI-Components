6c24f61433942feae5e437cf898156405f58829c
Tyler Biswell <tsbiswell@gmail.com>
2019-04-25 15:11:53 -0600

    pushing to correct repo haha

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <h1><strong>Lambda School</strong> <span>@LambdaSchool</span></h1>
+    </div>
+  )
+}
+
+export default HeaderTitle;
